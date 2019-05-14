bc956628e83fe1cce25eff4723c9b3f8890362b6
mec <davide.nunzio.maccarrone@gmail.com>
2019-05-14 13:44:11 +0200

    Integrazione modulo FileManager.m con funzionalità di acquisizione, salvataggio e modifica dati utente

--- Modules/File_manager.m
+++ Modules/File_manager.m
@@ -1,8 +1,44 @@
 (* ::Package:: *)
 
-CheckFile[directory_]:=Module[{},
-	(*Commento*)
+(*Directory di root progetto*)
+mainDirectory = "";
+userDataset = Null;
 
-	If[ FileExistsQ[ directory <> "Data/Users_data.dat" ], Print["Esiste"], Export[ directory <> "Data/Users_data.dat",  Null ] ];
-
+(*Metodo di inizializzazione, deve essere sempre richiamato prima di richiamare qualsiasi metodo*)
+Init[ directory_ ]:=Module[{},
+	mainDirectory = directory;
 ];
+
+(*Funzione che verifica se un determinato username, \[EGrave] presente nel dataset*)
+AddUser[ username_ ]:=Module[{usersDatasetList},
+
+	If[ FileExistsQ[ mainDirectory <> "Data/" <> username <> ".csv" ], 
+		(
+			usersDatasetList = Import[ mainDirectory <> "Data/" <> username <> ".csv", "CSV" ];
+						
+			
+			Print["Testsdfs"];
+			usersDatasetList = usersDatasetList[[1]];
+			userDataset = usersDatasetList;
+			Return[usersDatasetList];
+		)
+	, 
+		(
+			usersDatasetList = {username, 0, 0, 0};
+			Export[ mainDirectory <> "Data/" <> username <> ".csv",  {usersDatasetList} ]; 
+			userDataset = usersDatasetList;
+			Return [usersDatasetList];
+		)
+	 ];
+	 
+	 Return Null;
+	 
+];
+
+SaveUserData[ userData_ ]:=Module[{},
+   Export[ mainDirectory <> "Data/" <> userData[[1]] <> ".csv",  {userData} ];   
+   userDataset = userData;
+];
+
+
+
