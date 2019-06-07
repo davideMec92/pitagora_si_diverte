d8a993aa624c242325353f176315bf85df850c2b
mec <davide.nunzio.maccarrone@gmail.com>
2019-06-07 17:04:49 +0200

    Added login button controls

--- Modules/User_login.m
+++ Modules/User_login.m
@@ -1,47 +1,60 @@
 (* ::Package:: *)
 
+LoginDone = False;
+
 GetUserLoginGraphicInterface[]:=DynamicModule[{},
-	
-	Print[Style[ "Registrazione", "Title"]];
-	Print[Style[ "Inserisci il tuo nome nel campo seguente per salvare i tuoi progressi", "Text"]];
-	usernameText = Null;
-	
-	userLoginGrid = Grid[{
-		{Row[{
-			InputField[ Dynamic[usernameText], String ],
-			Button[
+
+	If[ LoginDone == False, (
+		Print[Style[ "Registrazione", "Title"]];
+		Print[Style[ "Inserisci il tuo nome nel campo seguente per salvare i tuoi progressi", "Text"]];
+		usernameText = Null;
+		loginButton = Button[
 					"Inserisci il tuo nome",
 					(
-						usernameText = ToLowerCase[usernameText];
-						userListData = AddUser[ usernameText ];						
-						Print[Style[ "Benvenuto/a " <> usernameText, "Text"]];
-						DisplayUserProgress[ userListData ];
+						If [ LoginDone == False, (
+							usernameText = ToLowerCase[usernameText];
+							userListData = AddUser[ usernameText ];						
+							Print[Style[ "Benvenuto/a " <> usernameText, "Text"]];
+							DisplayUserProgress[ userListData, loginButton ];
+						)
+						];
+						  
 					)
-				]
-		}]} 
-	}, Frame->Transparent];
+				];
+					
+		userLoginGrid = Grid[{
+			{Row[{
+				InputField[ Dynamic[usernameText], String ],
+				loginButton
+			}]} 
+		}, Frame->Transparent];
 	
-	Print[userLoginGrid];	
+		Print[userLoginGrid];
+	)];
 ];
 
-DisplayUserProgress[ userData_ ]:=Module[{},
+DisplayUserProgress[ userData_, loginButton_ ]:=Module[{},
 
-	Print[Style[ "Ecco i tuoi progressi", "Text"]];
-	mainDirectory = NotebookDirectory[] <> "Images/";
+	If[ LoginDone == False, (
+		Print[Style[ "Ecco i tuoi progressi", "Text"]];
+		mainDirectory = NotebookDirectory[] <> "Images/";
 	
-	ex1 = FileNameJoin[{ mainDirectory,"error.png"}];
-	ex2 = FileNameJoin[{ mainDirectory,"error.png"}];
-	ex3 = FileNameJoin[{ mainDirectory,"error.png"}];
+		ex1 = FileNameJoin[{ mainDirectory,"error.png"}];
+		ex2 = FileNameJoin[{ mainDirectory,"error.png"}];
+		ex3 = FileNameJoin[{ mainDirectory,"error.png"}];
 	
-	If[ GetExercise1[userData] == 1, ex1 = FileNameJoin[{ mainDirectory,"checked.png"}] ];
-	If[ GetExercise2[userData] == 1, ex2 = FileNameJoin[{ mainDirectory,"checked.png"}] ];
-	If[ GetExercise3[userData] == 1, ex3 = FileNameJoin[{ mainDirectory,"checked.png"}] ];	
+		If[ GetExercise1[userData] == 1, ex1 = FileNameJoin[{ mainDirectory,"checked.png"}] ];
+		If[ GetExercise2[userData] == 1, ex2 = FileNameJoin[{ mainDirectory,"checked.png"}] ];
+		If[ GetExercise3[userData] == 1, ex3 = FileNameJoin[{ mainDirectory,"checked.png"}] ];	
 	
-	userProgressgrid = Grid[{
-	{"Esercizio 1","Esercizio 2", "Esercizio 3"},
-	{Image[ Import[ex1], ImageSize->Tiny], Image[ Import[ex2], ImageSize->Tiny], Image[ Import[ex3], ImageSize->Tiny] } }, Frame->None ];
+		userProgressgrid = Grid[{
+		{"Esercizio 1","Esercizio 2", "Esercizio 3"},
+		{Image[ Import[ex1], ImageSize->Tiny], Image[ Import[ex2], ImageSize->Tiny], Image[ Import[ex3], ImageSize->Tiny] } }, Frame->None ];
 	
-	Print[userProgressgrid];
+		Print[userProgressgrid];
+		LoginDone = True;		
+	) ];
+	
 ];
 
 
