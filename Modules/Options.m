bd2455f80a8926d5355f93d1f56136ab15e78aeb
Bruno <bruno.quinteropanaro@studio.unibo.it>
2019-07-20 11:32:29 +0200

    Controlli pitagorici

--- Modules/Options.m
+++ Modules/Options.m
@@ -12,16 +12,88 @@ ModuleOptions[]:=Module[{},
 	SetOptions[EvaluationNotebook[],DockedCells->Cell[BoxData[ToBoxes[Grid[
 		{{
 			Import[FileNameJoin[{"Images","logo_small.png"}]], (* Logo a sinistra *)
-			ActionMenu[    (* ActionMenu al centro *)
+			ActionMenu[    (* ActionMenu al centro, quelli di pitagora hanno i controlli *)
 				"              Dove vuoi andare?             ",
 				{"Euclide Teorema" :> NotebookLocate["_EuclideEnunciato"],
 				 "Euclide Dimostrazione" :> NotebookLocate["_EuclideDimostrazione"],
 				 "Euclide Esempi" :> NotebookLocate["_EuclideEsempi"],
 				 "Euclide Esercizi" :> NotebookLocate["_EuclideEsercizi"],
-				 "Pitagora Teorema" :> NotebookLocate["_PitagoraEnunciato"],
-				 "Pitagora Dimostrazione" :> NotebookLocate["_PitagoraDimostrazione"],
-				 "Pitagora Esempi" :> NotebookLocate["_PitagoraEsempi"],
-				 "Pitagora Esercizi" :> NotebookLocate["_PitagoraEsercizi"]}
+				 "Pitagora Teorema" :> (
+						userSessionName = GetUserSession[];
+						
+						If[ userSessionName != "", ( 
+							
+							userData = AddUser[ userSessionName ];
+						
+							If[ userData[[GetIndexEx1Euclide[]]] == 0, (
+								MessageDialog[ "E' necessario completare il primo esercizio nella sezione relativa ad Euclide prima di procedere"];
+							), (
+								If[ userData[[GetIndexEx2Euclide[]]] == 0, (
+									MessageDialog[ "E' necessario completare il secondo esercizio nella sezione relativa ad Euclide prima di procedere"];
+								), (
+									NotebookLocate["_PitagoraEnunciato"];
+								)];
+							
+							) ];
+						)];
+					),
+				 "Pitagora Dimostrazione" :> (
+						userSessionName = GetUserSession[];
+						
+						If[ userSessionName != "", ( 
+							
+							userData = AddUser[ userSessionName ];
+						
+							If[ userData[[GetIndexEx1Euclide[]]] == 0, (
+								MessageDialog[ "E' necessario completare il primo esercizio nella sezione relativa ad Euclide prima di procedere"];
+							), (
+								If[ userData[[GetIndexEx2Euclide[]]] == 0, (
+									MessageDialog[ "E' necessario completare il secondo esercizio nella sezione relativa ad Euclide prima di procedere"];
+								), (
+									NotebookLocate["_PitagoraDimostrazione"];
+								)];
+							
+							) ];
+						)];
+					),
+				 "Pitagora Esempi" :> (
+						userSessionName = GetUserSession[];
+						
+						If[ userSessionName != "", ( 
+							
+							userData = AddUser[ userSessionName ];
+						
+							If[ userData[[GetIndexEx1Euclide[]]] == 0, (
+								MessageDialog[ "E' necessario completare il primo esercizio nella sezione relativa ad Euclide prima di procedere"];
+							), (
+								If[ userData[[GetIndexEx2Euclide[]]] == 0, (
+									MessageDialog[ "E' necessario completare il secondo esercizio nella sezione relativa ad Euclide prima di procedere"];
+								), (
+									NotebookLocate["_PitagoraEsempi"];
+								)];
+							
+							) ];
+						)];
+					),
+				 "Pitagora Esercizi" :> (
+						userSessionName = GetUserSession[];
+						
+						If[ userSessionName != "", ( 
+							
+							userData = AddUser[ userSessionName ];
+						
+							If[ userData[[GetIndexEx1Euclide[]]] == 0, (
+								MessageDialog[ "E' necessario completare il primo esercizio nella sezione relativa ad Euclide prima di procedere"];
+							), (
+								If[ userData[[GetIndexEx2Euclide[]]] == 0, (
+									MessageDialog[ "E' necessario completare il secondo esercizio nella sezione relativa ad Euclide prima di procedere"];
+								), (
+									NotebookLocate["_PitagoraEsercizi"];
+								)];
+							
+							) ];
+						)];
+					)}
 			], (* Bottoni a destra *)
 			Button["Home",NotebookLocate["_intro"],Background->White,FrameMargins->Medium],
 			Button["Utente",NotebookLocate["_user"],Background->White,FrameMargins->Medium],
@@ -39,6 +111,33 @@ ModuleOptions[]:=Module[{},
 
 ];
 
+
+
+Button[
+					Pitagora,
+					(
+						userSessionName = GetUserSession[];
+						
+						If[ userSessionName != "", ( 
+							
+							userData = AddUser[ userSessionName ];
+						
+							If[ userData[[GetIndexEx1Euclide[]]] == 0, (
+								MessageDialog[ "E' necessario completare il primo esercizio nella sezione relativa ad Euclide prima di procedere"];
+							), (
+								If[ userData[[GetIndexEx2Euclide[]]] == 0, (
+									MessageDialog[ "E' necessario completare il secondo esercizio nella sezione relativa ad Euclide prima di procedere"];
+								), (
+									PitagoraEsercizio1[];
+                                    PitagoraEsercizio2[];
+                                    PitagoraEsercizio3[];
+                                    PitagoraEsercizio4[];
+								)];
+							
+							) ];
+						)];
+					)
+				,ContentPadding -> False, FrameMargins->None, Background->White, Appearance->"Frameless"];
 
 
 
--- Modules/Welcome.m
+++ Modules/Welcome.m
@@ -3,6 +3,9 @@
 ModuleWelcome[]:=Module[{},
 (*Questa \[EGrave] la Home*)
 
+(*Mi servir\[AGrave] avere i dati utenti e di sessione*)
+Import["Modules/File_manager.m"];
+
 (*Immagini*)
 imgDirectory = NotebookDirectory[] <> "Images/";
 	
@@ -26,12 +29,33 @@ Print[Grid[{
 ];
 
 
-X=1;
+
 (*Pitagora ed Euclide*)
 Print[Grid[{
 		{"", 
 		Button[Euclide, NotebookLocate["_EuclideEnunciato"],ContentPadding -> False, FrameMargins->None, Background->White, Appearance->"Frameless"], 
-		Button[Pitagora, If[x>0, NotebookLocate["_PitagoraEnunciato"]], ContentPadding -> False, FrameMargins->None, Background->White, Appearance->"Frameless"]}
+		Button[
+					Pitagora,
+					(
+						userSessionName = GetUserSession[];
+						
+						If[ userSessionName != "", ( 
+							
+							userData = AddUser[ userSessionName ];
+						
+							If[ userData[[GetIndexEx1Euclide[]]] == 0, (
+								MessageDialog[ "E' necessario completare il primo esercizio nella sezione relativa ad Euclide prima di procedere"];
+							), (
+								If[ userData[[GetIndexEx2Euclide[]]] == 0, (
+									MessageDialog[ "E' necessario completare il secondo esercizio nella sezione relativa ad Euclide prima di procedere"];
+								), (
+									NotebookLocate["_PitagoraEnunciato"];
+								)];
+							
+							) ];
+						)];
+					)
+				,ContentPadding -> False, FrameMargins->None, Background->White, Appearance->"Frameless"]}
 	},Frame->Transparent, Alignment->Center, ItemSize -> {{Scaled[.15],Scaled[.3], Scaled[.3]}}, 
  Frame -> All]
 ];
