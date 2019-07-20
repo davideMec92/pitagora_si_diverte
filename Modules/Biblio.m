33cdda589feb82a4d854688dc77650174a827e40
Bruno <bruno.quinteropanaro@studio.unibo.it>
2019-07-20 15:07:50 +0200

    blue ridge mountains shenandoah river

---
+++ Modules/Biblio.m
@@ -0,0 +1,52 @@
+(* ::Package:: *)
+
+(*  
+
+  /$$$$$$$ /$$  /$$                                                                /$$             /$$/$$                             /$$             
+| $$__  $|__/ | $$                                                               |__/            | $|__/                            | $$             
+| $$  \ $$/$$/$$$$$$   /$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$      /$$$$$$$/$$        /$$$$$$$/$$/$$    /$$/$$$$$$  /$$$$$$ /$$$$$$   /$$$$$$ 
+| $$$$$$$| $|_  $$_/  |____  $$/$$__  $$/$$__  $$/$$__  |____  $$     /$$_____| $      /$$__  $| $|  $$  /$$/$$__  $$/$$__  $|_  $$_/  /$$__  $$
+| $$____/| $$ | $$     /$$$$$$| $$  \ $| $$  \ $| $$  \__//$$$$$$$     |  $$$$$$| $$     | $$  | $| $$\  $$/$$| $$$$$$$| $$  \__/ | $$   | $$$$$$$$
+| $$     | $$ | $$ /$$/$$__  $| $$  | $| $$  | $| $$     /$$__  $$      \____  $| $$     | $$  | $| $$ \  $$$/| $$_____| $$      | $$ /$| $$_____/
+| $$     | $$ |  $$$$|  $$$$$$|  $$$$$$|  $$$$$$| $$    |  $$$$$$$       /$$$$$$$| $$     |  $$$$$$| $$  \  $/ |  $$$$$$| $$       |  $$$$|  $$$$$$$
+|__/     |__/  \___/  \_______/\____  $$\______/|__/     \_______/      |_______/|__/       \_______|__/   \_/   \_______|__/        \___/  \_______/
+                               /$$  \ $$                                                                                                             
+                              |  $$$$$$/                                                                                                             
+                               \______/                                                                                                              
+
+Progetto d'esame di Matematica computazionale
+Anno accademico 2018-2019
+
+Sviluppato su Wolfram Mathematica 11.3
+							   
+							   
+Davide Nunzio Maccarrone (0000843825)
+Bruno Quintero Panaro 	 (0000850912)  
+*)
+
+
+ModuleBiblio[]:=Module[{},
+
+
+
+(* Bibliografia *)
+	
+Print[Style[ "Bibliografia","Subtitle"]];
+Print["\n\n"];
+
+Print[Style[ "- Manuale di Geometria, M.Bergamini, A.Trifone, G.Barozzi, Terza Edizione (Zanichelli 2010)","Text"]];
+Print["\n"];
+Print[Style[ "- Slides del corso di Matematica Computazionale, G.Spaletta (UNIBO 2019)","Text"]];
+Print["\n"];
+Print[Style[ "- Wolfram Language & System Documentation Center (Wolfram 2019)","Text"]];
+Print["\n"];
+Print[Style[ "- Formulari di geometria piana, YouMath (Math Industries Srl 2019)","Text"]];
+Print["\n"];
+Print[Style[ "- Le immagini di Pitagora ed Euclide sono di Teledipity SAPI de CV (2018)","Text"]];
+
+];
+
+
+
+
+
---
+++ Modules/Help.m
@@ -0,0 +1,56 @@
+(* ::Package:: *)
+
+(*  
+
+  /$$$$$$$ /$$  /$$                                                                /$$             /$$/$$                             /$$             
+| $$__  $|__/ | $$                                                               |__/            | $|__/                            | $$             
+| $$  \ $$/$$/$$$$$$   /$$$$$$  /$$$$$$  /$$$$$$  /$$$$$$    /$$$$$$      /$$$$$$$/$$       /$$$$$$$/$$/$$    /$$/$$$$$$  /$$$$$$ /$$$$$$   /$$$$$$ 
+| $$$$$$$| $|_  $$_/  |____  $$/$$__  $$/$$__  $$/$$__  |____  $$     /$$_____| $       /$$__  $| $|  $$  /$$/$$__  $$/$$__  $|_  $$_/  /$$__  $$
+| $$____/| $$ | $$     /$$$$$$| $$  \ $| $$  \ $| $$  \__//$$$$$$$     |  $$$$$$| $$     | $$  | $| $$\  $$/$$| $$$$$$$| $$  \__/ | $$   | $$$$$$$$
+| $$     | $$ | $$ /$$/$$__  $| $$  | $| $$  | $| $$     /$$__  $$      \____  $| $$     | $$  | $| $$ \  $$$/| $$_____| $$      | $$ /$| $$_____/
+| $$     | $$ |  $$$$|  $$$$$$|  $$$$$$|  $$$$$$| $$    |  $$$$$$$       /$$$$$$$| $$     |  $$$$$$| $$  \  $/ |  $$$$$$| $$       |  $$$$|  $$$$$$$
+|__/     |__/  \___/  \_______/\____  $$\______/|__/     \_______/      |_______/|__/       \_______|__/   \_/   \_______|__/        \___/  \_______/
+                               /$$  \ $$                                                                                                             
+                              |  $$$$$$/                                                                                                             
+                               \______/                                                                                                              
+
+Progetto d'esame di Matematica computazionale
+Anno accademico 2018-2019
+
+Sviluppato su Wolfram Mathematica 11.3
+							   
+							   
+Davide Nunzio Maccarrone (0000843825)
+Bruno Quintero Panaro 	 (0000850912)  
+*)
+
+
+ModuleHelp[]:=Module[{},
+
+
+
+(* Help *)
+	
+Print[Style[ "Help","Subtitle"]];
+Print["\n\n"];
+
+
+"Benvenuto in Pitagora si Diverte, per iniziare accedi al menu utente inserendo il tuo Nickname"<>"\n\n"<>"Poi, dalla home o dal menu in alto potrai accedere alla sessione di Eclide. "<>"\n\n"<>" Una volta completati i suoi esercizi potrai tornare procedere con la parte di Pitagora"
+
+
+Print[Style[ "Benvenuto in Pitagora si Diverte, di Davide Maccarrone e Bruno Q.Panaro per iniziare accedi al menu utente inserendo il tuo Nickname","Text"]];
+Print["\n"];
+Print[Style[ "Una volta fatto il log in sar\[AGrave] possibile procedere (dal menu home o dal menu a tendina in cima alle slides) nella sessione di Euclide","Text"]];
+Print["\n"];
+Print[Style[ "Ogni sessione \[EGrave] divisa in 4 gruppi di slide: Enunciato, Dimostrazione, Esempi ed Esercizi","Text"]];
+Print["\n"];
+Print[Style[ "Completare col proprio utente gli esercizi di Euclide \[EGrave] propedeutico per accedere alla sessione di Pitagora","Text"]];
+Print["\n"];
+Print[Style[ "Resta consigliato la visione delle slide in sequenza per poter comprendere al meglio il funzionamento dei teoremi prima di svolgere gli esercizi.","Text"]];
+
+];
+
+
+
+
+
